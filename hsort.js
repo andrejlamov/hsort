81c4cc5c62fe0728f3167df2d18ea0636bc76a37
Andrej Lamov <andrej.lamov@gmail.com>
2014-07-22 08:39:56 +0200

    Initial commit

---
+++ hsort.js
@@ -0,0 +1,36 @@
+Hsort = {
+  split: function(list, compare, access) {
+    if(list.length == 0) return [[]];
+
+    list.sort(function(a,b) {
+      return compare(access(a), access(b));
+    });
+
+    var splitted = [];
+    var pivot = list.splice(0, 1)[0];
+    splitted.push([pivot]);
+    var idx = 0;
+
+    list.forEach(function(d) {
+      if(access(d) == access(pivot)) {
+        splitted[idx].push(d);
+      } else {
+        splitted.push([d]);
+        pivot = d
+        idx++;
+      }
+    });
+    return splitted;
+  },
+
+  flatten: function() {
+    function flatten(list) {
+      return list.reduce(function(acc, d){
+               d.forEach(function(d0) {
+                 acc.push(d0);
+               })
+               return acc;
+             }, []);
+    }
+  }
+}
